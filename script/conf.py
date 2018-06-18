93c246126db18244bec6395f97777e8c7627422b
tristan.sebens <trisan.ng.sebens@gmail.com>
2018-06-18 09:46:20 -0800

    Added some configuration values

--- script/conf.py
+++ script/conf.py
@@ -1,7 +1,14 @@
-STATUS_STAGING = 1
+# The various states that the download can be in
+STATUS_STAGING = 0
+STATUS_CREATING_FILE = 1
 STATUS_ACTIVE = 2
 STATUS_COMATOSE = 3
 STATUS_DEAD = 4
 STATUS_COMPLETE = 5
+
+REPORT_WAIT = 0
+REPORT_DONE = 1
+REPORT_KILL = 3
+
 FILE_CREATION_TIMEOUT = 30 # Seconds the application will wait for a file to be created before throwing a FileWriteError
 MAX_ALLOWABLE_ERR_COUNT = 5 # Number of errors (of the same type) that can be thrown before a case is shelved
